f27956c52bfa7f58ef7629edb4c078fce85f105e
Sachin Saini <sachin.saini@ap2v.com>
2021-05-08 19:40:24 +0530

    v1

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt update
+sudo apt install openjdk-8-jdk -y
+sudo apt install jenkins -y
+sudo apt install terraform
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
